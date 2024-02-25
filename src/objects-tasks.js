f3e94e747c38802d284fbf17a3420127857c55ed
annaAzh <forarre@yandex.ru>
2024-02-25 17:03:06 +0300

    feat: solve makeWord task

--- src/objects-tasks.js
+++ src/objects-tasks.js
@@ -135,8 +135,15 @@ function makeImmutable(obj) {
  *    makeWord({ a: [0, 1], b: [2, 3], c: [4, 5] }) => 'aabbcc'
  *    makeWord({ H:[0], e: [1], l: [2, 3, 8], o: [4, 6], W:[5], r:[7], d:[9]}) => 'HelloWorld'
  */
-function makeWord(/* lettersObject */) {
-  throw new Error('Not implemented');
+function makeWord(lettersObject) {
+  const result = [];
+  Object.entries(lettersObject).forEach(([value, key]) => {
+    key.forEach((index) => {
+      result[index] = value;
+    });
+  });
+
+  return result.join('');
 }
 
 /**
